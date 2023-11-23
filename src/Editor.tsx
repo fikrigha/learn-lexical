cb2e49a1c067bb7edf81203d5e8708ef08e09ee5
fikrigha <fikrigha@gmail.com>
2023-11-23 15:29:39 -0500

    Add a simple plugin

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,18 +1,36 @@
-import { $getRoot, $getSelection } from "lexical";
+import { EditorState } from "lexical";
 import { useEffect } from "react";
 
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
-import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+
+import "./Editor.css";
 
 const theme = {};
 
 function onError(error: Error) {
   console.log(error);
+}
+
+function MyOnChangePlugin(props: {
+  onChange: (editorState: EditorState) => void;
+}) {
+  console.log("MyOnChangePlugin");
+
+  // Access the editor instance
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    return editor.registerUpdateListener(({ editorState }) => {
+      props.onChange(editorState);
+    });
+  }, [props.onChange, editor]);
+
+  return null;
 }
 
 export default function Editor() {
@@ -23,12 +41,20 @@ export default function Editor() {
   };
 
   return (
-    <LexicalComposer initialConfig={initialConfig}>
-      <PlainTextPlugin
-        contentEditable={<ContentEditable />}
-        placeholder={<div>Enter some text...</div>}
-        ErrorBoundary={LexicalErrorBoundary}
-      />
-    </LexicalComposer>
+    <div className="editorWrapper">
+      <LexicalComposer initialConfig={initialConfig}>
+        <PlainTextPlugin
+          contentEditable={<ContentEditable className="contentEditable" />}
+          placeholder={<div className="placeholder">Enter some text...</div>}
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+        <HistoryPlugin />
+        <MyOnChangePlugin
+          onChange={(editorState) => {
+            console.log("<MyOnChangePlugin/>", editorState);
+          }}
+        />
+      </LexicalComposer>
+    </div>
   );
 }
