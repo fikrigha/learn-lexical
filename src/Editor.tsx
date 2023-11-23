3cddddab16619ff5d3cec52f4a4007115c77406b
fikrigha <fikrigha@gmail.com>
2023-11-23 15:03:05 -0500

    Clean up, add basic editor

---
+++ src/Editor.tsx
@@ -0,0 +1,34 @@
+import { $getRoot, $getSelection } from "lexical";
+import { useEffect } from "react";
+
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+
+const theme = {};
+
+function onError(error: Error) {
+  console.log(error);
+}
+
+export default function Editor() {
+  const initialConfig = {
+    namespace: "MyEditor",
+    theme,
+    onError,
+  };
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <PlainTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div>Enter some text...</div>}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+    </LexicalComposer>
+  );
+}
