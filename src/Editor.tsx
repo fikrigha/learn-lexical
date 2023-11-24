ff9b8bdee922852c0bf60cd1382c8da784965c8e
fikrigha <fikrigha@gmail.com>
2023-11-24 18:03:05 -0500

    Test rich text

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,36 +1,43 @@
-import { EditorState } from "lexical";
+import { $createTextNode, $getRoot, EditorState } from "lexical";
 import { useEffect } from "react";
 
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
-import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+import { HeadingNode, $createHeadingNode } from "@lexical/rich-text";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 
 import "./Editor.css";
 
-const theme = {};
+// https://lexical.dev/docs/getting-started/theming
+const theme = {
+  heading: {
+    h1: "editor-h1",
+  },
+  text: {
+    bold: "editor-bold",
+  },
+};
+
+function MyHeadingPlugin() {
+  const [editor] = useLexicalComposerContext();
+
+  const onClick = (e: React.MouseEvent) => {
+    editor.update(() => {
+      const root = $getRoot();
+      root.append(
+        $createHeadingNode("h1").append($createTextNode("hello world"))
+      );
+    });
+  };
+
+  return <button onClick={onClick}>Heading</button>;
+}
 
 function onError(error: Error) {
   console.log(error);
-}
-
-function MyOnChangePlugin(props: {
-  onChange: (editorState: EditorState) => void;
-}) {
-  console.log("MyOnChangePlugin");
-
-  // Access the editor instance
-  const [editor] = useLexicalComposerContext();
-
-  useEffect(() => {
-    return editor.registerUpdateListener(({ editorState }) => {
-      props.onChange(editorState);
-    });
-  }, [props.onChange, editor]);
-
-  return null;
 }
 
 export default function Editor() {
@@ -38,22 +45,19 @@ export default function Editor() {
     namespace: "MyEditor",
     theme,
     onError,
+    nodes: [HeadingNode],
   };
 
   return (
     <div className="editorWrapper">
       <LexicalComposer initialConfig={initialConfig}>
-        <PlainTextPlugin
+        <MyHeadingPlugin />
+        <RichTextPlugin
           contentEditable={<ContentEditable className="contentEditable" />}
           placeholder={<div className="placeholder">Enter some text...</div>}
           ErrorBoundary={LexicalErrorBoundary}
         />
         <HistoryPlugin />
-        <MyOnChangePlugin
-          onChange={(editorState) => {
-            console.log("<MyOnChangePlugin/>", editorState);
-          }}
-        />
       </LexicalComposer>
     </div>
   );
