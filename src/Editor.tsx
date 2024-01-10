6ded0de271dc3908e0d1cb0b954cf502b16d344d
fikrigha <fikrigha@gmail.com>
2024-01-09 22:22:21 -0500

    Test

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,5 +1,16 @@
-import { $createTextNode, $getRoot, EditorState } from "lexical";
-import { useEffect } from "react";
+import {
+  $getSelection,
+  $isRangeSelection,
+  COMMAND_PRIORITY_LOW,
+} from "lexical";
+import { $setBlocksType } from "@lexical/selection";
+import {
+  INSERT_ORDERED_LIST_COMMAND,
+  INSERT_UNORDERED_LIST_COMMAND,
+  ListNode,
+  ListItemNode,
+  insertList,
+} from "@lexical/list";
 
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
@@ -21,19 +32,53 @@ const theme = {
   },
 };
 
-function MyHeadingPlugin() {
+function ToolbarPlugin() {
+  return (
+    <div>
+      <HeadingPlugin />
+      <ListPlugin />
+    </div>
+  );
+}
+
+function HeadingPlugin() {
   const [editor] = useLexicalComposerContext();
 
-  const onClick = (e: React.MouseEvent) => {
+  const onClick = (tag: "h1" | "h2" | "h3"): void => {
     editor.update(() => {
-      const root = $getRoot();
-      root.append(
-        $createHeadingNode("h1").append($createTextNode("hello world"))
-      );
+      const selection = $getSelection();
+      if ($isRangeSelection(selection)) {
+        $setBlocksType(selection, () => $createHeadingNode(tag));
+      }
     });
   };
 
-  return <button onClick={onClick}>Heading</button>;
+  return (
+    <>
+      <button onClick={() => onClick("h1")}>H1</button>
+      <button onClick={() => onClick("h2")}>H2</button>
+      <button onClick={() => onClick("h3")}>H3</button>
+    </>
+  );
+}
+
+function ListPlugin() {
+  const [editor] = useLexicalComposerContext();
+
+  const onClick = (tag: "ol" | "ul"): void => {
+    if (tag === "ol") {
+      editor.dispatchCommand(INSERT_ORDERED_LIST_COMMAND, undefined);
+      return;
+    }
+    editor.dispatchCommand(INSERT_UNORDERED_LIST_COMMAND, undefined);
+  };
+
+  return (
+    <>
+      <button onClick={() => onClick("ol")}>ol</button>
+      <button onClick={() => onClick("ul")}>ul</button>
+    </>
+  );
 }
 
 function onError(error: Error) {
@@ -45,13 +90,15 @@ export default function Editor() {
     namespace: "MyEditor",
     theme,
     onError,
-    nodes: [HeadingNode],
+    nodes: [HeadingNode, ListNode, ListItemNode],
   };
 
   return (
     <div className="editorWrapper">
       <LexicalComposer initialConfig={initialConfig}>
-        <MyHeadingPlugin />
+        <div>
+          <ToolbarPlugin />
+        </div>
         <RichTextPlugin
           contentEditable={<ContentEditable className="contentEditable" />}
           placeholder={<div className="placeholder">Enter some text...</div>}
